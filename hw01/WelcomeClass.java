acbdac1f8da291381736612bcf0bf4d971e9a0d5
Cailee Cassidy <cjc216@lehigh.edu>
2014-09-02 19:06:52 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,19 @@
+/////////////////////////////////
+//Cailee Cassidy
+//hw01
+//Welcome Class Java Program
+//
+public class WelcomeClass {
+    public static void main(String[] args) {
+        
+        System.out.println("       -----------       ");
+        System.out.println("      |  WELCOME  |      "); 
+        System.out.println("       -----------       ");
+        System.out.println("  ^   ^   ^   ^   ^   ^  ");
+        System.out.println(" / \\ /\\ /\\ /\\ /\\ /\\ ");
+        System.out.println("|-C---J---C---2---1---6-|");
+        System.out.println(" \\/\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v   v   v   v   v   v  ");
+        System.out.println("My name is Cailee Cassidy, I'm a junior at Lehigh University in Bethlehem, PA, and my favorite animal is a shark.");
+    }
+}
