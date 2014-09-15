cf29426724b091b421b5241fb2577b184d1c1949
Cailee Cassidy <cjc216@lehigh.edu>
2014-09-15 23:32:01 +0000

    updated Bicycle, better comments

---
+++ hw03/Root.java
@@ -0,0 +1,7 @@
+////////////////////////////////////////////////////////////////////////////////////////////
+//Cailee Cassidy
+//CSE 002 HW 03
+//Due 9/16/14
+//Practice in writing code that allows user to input data, 
+//practice in performing arithmetic operations. This program
+//asks the user to input 
