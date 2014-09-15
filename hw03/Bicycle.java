18464c1191eb810bec8887b6b3edf1ccda355a33
Cailee Cassidy <cjc216@lehigh.edu>
2014-09-15 23:25:32 +0000

    updated Bicycle

---
+++ hw03/Bicycle.java
@@ -0,0 +1,33 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Cailee Cassidy
+//CSE 002 HW 03
+//Due 9/16/14
+//Practice in writing code that allows user to input data, 
+//practice in performing arithmetic operations
+import java.util.Scanner;
+public class Bicycle {
+    public static void main(String[] args) {
+        //Create a scanner
+        Scanner input = new Scanner(System.in);
+        //Enter number of seconds
+        System.out.println("Enter the number of seconds:");
+        int numberSeconds = input.nextInt();
+        //Enter number of counts
+        System.out.println("Enter the number of counts:");
+        int numberCounts = input.nextInt();
+        //Input variables
+        double wheelDiameter = 27.0;
+        double PI = 3.14159;
+        int feetPerMile = 5280;
+        int inchesPerFoot = 12;
+        int secondsPerMinute = 60;
+        int minutesPerHour = 60;
+        //Calculate distance
+        double distanceTrip = Math.round(numberCounts*wheelDiameter*PI/inchesPerFoot/feetPerMile*100.00)/100.00;
+        //Gives the distance in miles
+        double timeTrip = numberSeconds/secondsPerMinute;
+        System.out.println("The distance was "+distanceTrip+" miles and took "+timeTrip+" minutes.");
+        double avgMilesPerHour = Math.round((distanceTrip/timeTrip)*minutesPerHour*100.00)/100.00;
+        System.out.println("The average mph was "+avgMilesPerHour);
+    }
+}
